4fc3d12d0ff8f3f5a234c0eaeb11e74ec0380754
Slavov <kostadin@slavov.net>
2022-10-05 23:23:52 +0300

    Pull the map from the ws

--- public/js/ws_client.js
+++ public/js/ws_client.js
@@ -1,10 +1,18 @@
 const socket = new WebSocket('ws://localhost:3000')
-
+var map = null
 socket.addEventListener('open', function (event){
     socket.send('Hello from client!')
 })
 
 socket.addEventListener('message', function (event){
+    let data = event.data.toString()
+    if( event.data.toString().split(':')[0] === 'MAP'){
+        let map_info = []
+        for (let i=0; i<data.split(',').length; i++){
+            map_info.push((data.split(',')[i]).split('|'))
+        }
+        map = map_info
+    }
     console.log('Message from server ', event.data)
 })
 
